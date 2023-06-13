94f77a1397db2b05a8cecfde7e1a05041f645649
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2023-06-13 13:09:42 +0300

    push loader action to events file

--- Project6_Routing/Project6_2_Routing_Advanced/frontend/src/App.js
+++ Project6_Routing/Project6_2_Routing_Advanced/frontend/src/App.js
@@ -1,27 +1,6 @@
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
@@ -39,18 +18,9 @@ const router = createBrowserRouter([
                 children: [
                     {
                         index: true, element: <EventsPage />,
-                        loader: async () => {
-                            // эта функция будет вызываться при каждом посещении страницы перед рендером 
-                            // async возвращает промис и реакт чекает этот промис и возвращает дату через хук  useLoaderData()
-                            const response = await fetch('http://localhost:8080/events');
-
-                            if (!response.ok) {
-                                // ...
-                            } else {
-                                const resData = await response.json();
-                                return resData.events;
-                            }
-                        },
+                        loader: eventsLoader,
+                        // эта функция будет вызываться при каждом посещении страницы перед рендером 
+                        // async возвращает промис и реакт чекает этот промис и возвращает дату через хук  useLoaderData()
                     },
                     { path: ":eventId", element: <EventDetailPage /> },
                     { path: "new", element: <NewEventPage /> },
