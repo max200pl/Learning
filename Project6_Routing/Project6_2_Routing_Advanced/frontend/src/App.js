13c7ec3deb1ebf0fb56f23243216dde02aa7b991
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2023-06-13 12:27:38 +0300

    added basic navigation

--- Project6_Routing/Project6_2_Routing_Advanced/frontend/src/App.js
+++ Project6_Routing/Project6_2_Routing_Advanced/frontend/src/App.js
@@ -19,9 +19,50 @@
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
+import EventsRootLayout from "./pages/EventsRoot";
+
+const router = createBrowserRouter([
+    {
+        path: "/", element: <RootLayout />, children: [
+            { index: true, element: <HomePage /> },
+            {
+                path: "events",
+                element: <EventsRootLayout />,
+
+                children: [
+                    {
+                        index: true, element: <EventsPage />,
+                        loader: async () => {
+                            // эта функция будет вызываться при каждом посещении страницы перед рендером 
+                            // async возвращает промис и реакт чекает этот промис и возвращает дату через хук  useLoaderData()
+                            const response = await fetch('http://localhost:8080/events');
+
+                            if (!response.ok) {
+                                // ...
+                            } else {
+                                const resData = await response.json();
+                                return resData.events;
+                            }
+                        },
+                    },
+                    { path: ":eventId", element: <EventDetailPage /> },
+                    { path: "new", element: <NewEventPage /> },
+                    { path: ":eventId/edit", element: <EditEventPage /> },
+                ]
+            },
+        ]
+    },
+])
 
 function App() {
-  return <div></div>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
