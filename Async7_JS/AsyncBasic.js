137e109b9b61788fd89dde5488235cc81cda6591
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2023-08-07 23:12:03 +0300

    Promises added

--- Async7_JS/AsyncBasic.js
+++ Async7_JS/AsyncBasic.js
@@ -66,4 +66,94 @@ calculateSquare(1, function (err, result) {
 
 module.exports = calculateSquare;
 
+//* ========= Promise and promisify function  =========
 
+// Promise is a special JavaScript object that represents an eventual result of asynchronous action
+// Promise has two enteral properties:
+// PromiseStatus and PromiseValue 
+// PromiseValue is contained value if successful 
+// PromiseStatus has 3 status: pending, fulfilled, rejected
+
+const myPromise = new Promise(function (resolve, reject) { // create instance of this class. Promise has one argument executor function
+    resolve("Hello World"); // only one status have to be fulfilled or rejected 
+    resolve("value2"); //! not working 
+    reject("reason")   //! not working 
+})
+
+console.log(myPromise) // 
+
+// then method takes 2 arguments:
+// onFulfilled
+// onRejected
+
+myPromise.then(function (value) { // onFulfilled function  Asynchronously function
+    console.log("onFulfilled" + value)
+    console.log('This is inside onFulfilled function')
+});
+
+console.log("This is written after myPromise.then")
+
+function calculateSquarePromise(number) {
+    const promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            if (typeof number !== "number") {
+                return reject(new Error("Argument must be a number"));
+            }
+            const result = number * number;
+            resolve(result);
+        }, 1000)
+    });
+
+    return promise;
+}
+
+calculateSquarePromise(2)
+    .then(value => {
+        console.log("Success: " + value);
+    }, reason => {
+        console.log("Error: " + reason);
+    })
+
+calculateSquarePromise("2")
+    .then(value => {
+        console.log("Success: " + value);
+    }, reason => {
+        console.log("Error: " + reason);
+    })
+
+function capitalize(str) {
+    return new Promise((resolve, reject) => {
+        if (typeof str !== "string") {
+            return reject(new Error("Not a string"));
+        }
+        const result = str.charAt(0).toUpperCase();
+        return resolve(result)
+    })
+}
+
+function successesAction(action) {
+    return new Promise((resolve, reject) => {
+        if (action === "success") {
+            return resolve(action)
+        }
+        if (action == "fail") {
+            return reject(new Error("Fail" + action))
+        }
+    })
+}
+
+calculateSquarePromise(1)
+    .then((value1) => {
+        console.log("Success" + value1)
+        // return 24
+        // throw new Error("error");
+        // return calculateSquarePromise(2);
+        return calculateSquarePromise("2");
+    })
+    .then((value2) => {
+        console.log("Previous then" + value2)
+    }, (reason) => {
+        console.log("Error" + reason)
+    });
+
+//* __________ // Promise and promisify function  __________
