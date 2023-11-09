0a7db9f20484b0a0bd7ede7c24a50a232f48c34d
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2023-11-09 12:57:10 +0200

    added initial planets before loading server

--- Node_JS_Course/6. nasa_project/server/src/models/planets.model.js
+++ Node_JS_Course/6. nasa_project/server/src/models/planets.model.js
@@ -1,3 +1,69 @@
-const planets = [];
+// fs-> allows open file reading and writing
+// fs -> reading file pice by peace
+const fs = require("fs");
 
-module.exports = planets;
+// path -> relative path __dirname
+const path = require("path");
+
+const { parse } = require("csv-parse");
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === "CONFIRMED"
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+/**
+ const promise = new Promise((res, reject)=>{
+    resolve(42)
+ });
+
+ promise.then((result)=>{
+
+ });
+    ========= OR =========
+ const result = await promise;
+ */
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+
+        fs.createReadStream(path.join(__dirname, "..", "..", "data", "kepler_data.csv")) // 1. represent collection of bites   <Buffer 23 20 54 68 69 73 20  61 6e 65 74 20 41 72 63 ... 65486 more bytes>,
+            .pipe(
+                parse( // pipe connection two string together
+                    {
+                        comment: "#",
+                        columns: true,
+                    }
+                )
+            )
+            .on("data", (data) => {
+                if (isHabitablePlanet(data)) {
+                    habitablePlanets.push(data);
+                }
+            })
+
+            .on("error", (err) => {
+                console.log(err);
+                //! REJECT FUNCTION */
+                reject(err);
+            })
+
+            .on("end", () => {
+                console.log(`${habitablePlanets.length} habitable planets`);
+                console.log("done");
+                //*** RESOLVE FUNCTION */
+                resolve() // whe now when are planets data has been successfully loaded
+            });
+    })
+}
+
+// parse(); // returns event emitter
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+}
--- Node_JS_Course/6. nasa_project/server/src/server.js
+++ Node_JS_Course/6. nasa_project/server/src/server.js
@@ -2,11 +2,26 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
 const sever = http.createServer(app);
 
-sever.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`);
-})
+async function startServer() {
+    // preform some actions before starting the server:
+    // 1. download some files
+    // 2. load some data base
+    // ...
+
+    //* wait load planets before starting server
+    await loadPlanetsData();
+
+    sever.listen(PORT, () => {
+        console.log(`Listening on ${PORT}...`);
+    })
+}
+
+startServer();
+// ...
 
