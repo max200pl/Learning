3fe77d4ee2cc16eaa98fd75c2bfe76ee82bb6e7b
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2023-11-10 17:10:57 +0200

    added some data validation

--- Node_JS_Course/6. nasa_project/server/src/routes/launches/launches.controller.js
+++ Node_JS_Course/6. nasa_project/server/src/routes/launches/launches.controller.js
@@ -9,8 +9,18 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
-    console.log(launch, "launch");
+
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+        return res.status(400).json({
+            error: "Missing required launch property",
+        })
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({ error: "Invalid launch date" });
+    }
 
     addNewLaunch(launch)
     return res.status(201).json(launch); // if successful return launch
