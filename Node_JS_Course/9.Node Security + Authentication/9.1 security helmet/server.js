e339acc150604952682e297e3ff0fb76bf716ca7
max200pl <max2000pl@gmail.com>
2024-01-05 10:17:21 +0200

    added authentication

--- Node_JS_Course/9.Node Security + Authentication/9.1 security helmet/server.js
+++ Node_JS_Course/9.Node Security + Authentication/9.1 security helmet/server.js
@@ -12,11 +12,62 @@ require('dotenv').config();
 
 const PORT = 3000;
 
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
+
+const AUTH_OPTIONS = {
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log("Google profile", profile);
+    done(null, profile); // first parameter is error
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const app = express();
 app.use(helmet());
 
-app.get('/secret', (req, res) => {
+app.use(passport.initialize());
+
+function checkLoggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.status(401).send('Not authenticated');
+    }
+}
+
+app.get("/auth/google",
+    passport.authenticate('google', {
+        scope: ["email"],
+    })
+);
+
+app.get("/auth/google/callback",
+    passport.authenticate('google', {
+        successRedirect: '/',
+        failureRedirect: '/failure',
+        session: false,
+    }),
+    (req, res) => {
+        console.log("Google called us back");
+    }
+);
+
+app.get("/auth/logout", (req, res) => { });
+
+app.get('/secret', checkLoggedIn, (req, res) => {
     return res.send('Secret page');
+});
+
+app.get('/failure', (req, res) => {
+    return res.send('Failed to authenticate');
 });
 
 app.get("/", (req, res) => {
