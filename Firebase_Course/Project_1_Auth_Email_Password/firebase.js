31fd4991d70877cc467c584fb033edbd96f2b421
max200pl <max2000pl@gmail.com>
2025-01-23 11:06:48 +0200

    #Firebase authentication #add initial setup for Firebase authentication web app with HTML, CSS, and JavaScript

---
+++ Firebase_Course/Project_1_Auth_Email_Password/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpGKZBADI_zwx62MtgCEaxygPUqV0q2Ho",
+  authDomain: "learnfirebase-cfdf8.firebaseapp.com",
+  projectId: "learnfirebase-cfdf8",
+  storageBucket: "learnfirebase-cfdf8.firebasestorage.app",
+  messagingSenderId: "438404034047",
+  appId: "1:438404034047:web:7074d63cd142bbfc090bca",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ Firebase_Course/User Auth with Email&Password/Fundamentals.md
@@ -0,0 +1,10 @@
+# Fundamentals concepts of  Firebase
+
+- [Fundamentals concepts of  Firebase](#fundamentals-concepts-of--firebase)
+  - [Create A Firebase Project Firebase Console](#create-a-firebase-project-firebase-console)
+
+## Create A Firebase Project Firebase Console
+
+## Create Web App
+
+## Add Firebase SDK
