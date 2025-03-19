2a92c93f0efafd2616b15738328dcbe2031946d0
max200pl <max2000pl@gmail.com>
2025-03-19 08:22:27 +0200

    #Rust #Project_1 #enhance deck initialization with suits and values, and improve output formatting

--- Rust/Project_1_Deck/src/main.rs
+++ Rust/Project_1_Deck/src/main.rs
@@ -5,7 +5,23 @@ struct Deck {
 }
 
 fn main() {
-    let deck: Deck = Deck { cards: vec![] };
+    // List of suits - Spades, Hearts, Diamonds, Clubs
+    // List of values - 2-10, J, Q, K, A
 
-    println!("Heres your deck: {:?}", deck);
+    let suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+    let values = ["Ace", "Two", "Three"];
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            println!("{} of {}", value, suit);
+
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    let deck: Deck = Deck { cards };
+
+    println!("Heres your deck: {:#?}", deck);
 }
