ac428f72bb3b5ecffc3c48ba8154bf951aef5df8
max200pl <max2000pl@gmail.com>
2025-03-28 10:06:43 +0200

    #Electron #Project_4_Video_Converter #add functionality to open output folder after video conversion

--- Electron/Project_4_Video_Converter/src/actions/index.js
+++ Electron/Project_4_Video_Converter/src/actions/index.js
@@ -44,7 +44,9 @@ export const convertVideos = () => (dispatch, getState) => {
 
 // TODO: Open the folder that the newly created video
 // exists in
-export const showInFolder = (outputPath) => (dispatch) => {};
+export const showInFolder = (outputPath) => (dispatch) => {
+  ipcRenderer.send("folder:open", outputPath);
+};
 
 export const addVideo = (video) => {
   return {
