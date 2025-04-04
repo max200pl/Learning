66da37c651436b4bef6cdff6978730c0746fa1cf
max200pl <max2000pl@gmail.com>
2025-04-04 09:49:24 +0300

    #Angular  #Project_1_Essential #42. Outputting Conditional Content

--- Angular/Angular2/Project_1_Essentials/src/app/app.component.ts
+++ Angular/Angular2/Project_1_Essentials/src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TasksComponent } from './tasks/tasks.component';
 export class AppComponent {
   users = DUMMY_USERS;
 
-  selectedUserId = 'u1';
+  selectedUserId = '';
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
