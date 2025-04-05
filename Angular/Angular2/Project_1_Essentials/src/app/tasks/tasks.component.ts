e1dca83a9dd24278c578e6eb3161e474930bc440
max200pl <max2000pl@gmail.com>
2025-04-05 16:20:37 +0300

    #Angular  #Project_1_Essential #51. Managing The "New Task" Dialog

--- Angular/Angular2/Project_1_Essentials/src/app/tasks/tasks.component.ts
+++ Angular/Angular2/Project_1_Essentials/src/app/tasks/tasks.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from '../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask = false;
 
   tasks = dummyTasks;
 
@@ -23,5 +25,15 @@ export class TasksComponent {
     console.log('Task Completed:', id);
 
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    console.log('Add Task Clicked');
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    console.log('Cancel Add Task Clicked');
+    this.isAddingTask = false;
   }
 }
