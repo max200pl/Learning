30979d8658f5f629d1600ed630ba6c5b56b7c9fd
max200pl <max2000pl@gmail.com>
2025-04-07 09:13:34 +0300

    #Angular  #Project_1_Essential #62. Using localStorage for Data Storage

--- Angular/Angular2/Project_1_Essentials/src/app/tasks/tasks.service.ts
+++ Angular/Angular2/Project_1_Essentials/src/app/tasks/tasks.service.ts
@@ -7,6 +7,14 @@ import { Task } from './task/task.model';
 export class TaskService {
   private tasks: Task[] = dummyTasks;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -19,11 +27,19 @@ export class TaskService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     console.log('Task Completed:', id);
 
     this.tasks = this.tasks.filter((task) => task.id !== id);
+
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
