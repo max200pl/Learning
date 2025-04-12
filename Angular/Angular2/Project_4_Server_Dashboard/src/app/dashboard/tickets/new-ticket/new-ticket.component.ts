2f1a549789f35136717ec45e8818040e86bea239
max200pl <max2000pl@gmail.com>
2025-04-12 13:28:38 +0300

    #Angular #Project_4_Server_Dashboard #132. Using The viewChild Signal Function

--- Angular/Angular2/Project_4_Server_Dashboard/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Angular/Angular2/Project_4_Server_Dashboard/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -12,29 +12,46 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
   //* Example 1: of a simple ticket creation form
-  /*   onSubmit(titleElement: HTMLInputElement) {
-    console.dir(titleElement);
-    console.log('Ticket submitted:' + titleElement.value);
-    // Here you would typically send the ticket data to a server or perform some action
-  }
+  /*
+    onSubmit(titleElement: HTMLInputElement) {
+      console.dir(titleElement);
+      console.log('Ticket submitted:' + titleElement.value);
+      // Here you would typically send the ticket data to a server or perform some action
+    }
  */
 
   //* EXAMPLE 2: Using Angular Forms
-  /*   onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
-    console.log('Ticket submitted:', { title, ticketText });
-    // Here you would typically send the ticket data to a server or perform some action
+  /*
+    onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
+      console.log('Ticket submitted:', { title, ticketText });
+      // Here you would typically send the ticket data to a server or perform some action
 
-    // Reset the form after submission
-    form.reset();
-  } */
+      // Reset the form after submission
+      form.reset();
+    }
+  */
 
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>; // #form
+  //* EXAMPLE 3: Using Angular Forms with ViewChild
+  /*
+    @ViewChild('form') form?: ElementRef<HTMLFormElement>; // #form
+
+    onSubmit(title: string, ticketText: string) {
+      console.log('Ticket submitted:', { title, ticketText });
+      // Here you would typically send the ticket data to a server or perform some action
+
+      // Reset the form after submission
+      this.form?.nativeElement.reset();
+    }
+  */
+
+  //* EXAMPLE 4: (New From 17.3) Using Angular Forms with viewChild and ElementRef
+  private form = viewChild.required<HTMLFormElement>('form');
 
   onSubmit(title: string, ticketText: string) {
     console.log('Ticket submitted:', { title, ticketText });
     // Here you would typically send the ticket data to a server or perform some action
 
     // Reset the form after submission
-    this.form?.nativeElement.reset();
+    this.form()?.reset();
   }
 }
