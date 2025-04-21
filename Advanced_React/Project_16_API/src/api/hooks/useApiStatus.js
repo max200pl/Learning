022a91cffefc1360263590b9b607e3dac2f34fad
max200pl <max2000pl@gmail.com>
2025-04-21 14:30:02 +0300

    #Advanced_React  #Project_16_API #Abstracting API States and Fetching Logic

--- Advanced_React/Project_16_API/src/api/hooks/useApiStatus.js
+++ Advanced_React/Project_16_API/src/api/hooks/useApiStatus.js
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState, useMemo } from "react";
 import { IDLE, defaultApiStatuses } from "../../constants/api-status";
 
 const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
