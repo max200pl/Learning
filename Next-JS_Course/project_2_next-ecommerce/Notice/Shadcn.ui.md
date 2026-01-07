b9477bf461c20a4f07432bda8358defe9bb43544
Maksym Poskannyi <max2000pl@gmail.com>
2026-01-07 22:26:00 +0200

    #Next_Js #eCommerce #Connect  ui.shadcn

---
+++ Next-JS_Course/project_2_next-ecommerce/Notice/Shadcn.ui.md
@@ -0,0 +1,19 @@
+# Shadcn/ui
+
+<https://ui.shadcn.com/docs/installation/next>
+
+Shadcn/ui is a popular open-source component library for building user interfaces in React applications. It provides a collection of pre-designed and customizable UI components that follow modern design principles and best practices. Shadcn/ui is known for its simplicity, flexibility, and ease of use, making it a great choice for developers looking to quickly create visually appealing and responsive web applications.
+
+To get started with Shadcn/ui, you can follow these steps:
+
+## Step 1: Install Shadcn/ui
+
+```bash
+npx shadcn@latest init
+```
+
+## Step 2: Add components to your project
+
+```bash
+npx shadcn@latest add button card badge skeleton
+```
--- Next-JS_Course/project_2_next-ecommerce/app/ProductCard.tsx
+++ Next-JS_Course/project_2_next-ecommerce/app/ProductCard.tsx
@@ -1,23 +1,38 @@
-import { Product } from "@/lib/mocks";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { formatPrice } from "@/lib/utils";
+
 import Image from "next/image";
+import { Product } from "./generated/prisma/client";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
-    <div className="border border-gray-200 rounded-lg p-4">
+    <Card className="pt-0 overflow-hidden">
       <div className="relative aspect-video">
-        <Image
-          src={product.image}
-          alt={product.name}
-          className="object-cover"
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
+        {product.image && (
+          <Image
+            src={product.image}
+            alt={product.name}
+            className="object-cover"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        )}
       </div>
 
-      <h2 className="text-lg font-semibold">{product.name}</h2>
-      <p className="text-gray-600">{formatPrice(product.price)}</p>
-      <p className="text-gray-500">{product.description}</p>
-    </div>
+      <CardHeader>
+        <CardTitle>{product.name}</CardTitle>
+        <CardDescription>{product.description}</CardDescription>
+      </CardHeader>
+
+      <CardFooter>
+        <p>{formatPrice(product.price)}</p>
+      </CardFooter>
+    </Card>
   );
 }
