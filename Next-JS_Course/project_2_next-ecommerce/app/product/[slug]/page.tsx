07103ac9d4a62de241d649015e77725d15f7da12
Maksym Poskannyi <max2000pl@gmail.com>
2026-01-08 18:31:52 +0200

    #Next_Js #eCommerce #Add sleep utility function and integrate image display in ProductPage component

--- Next-JS_Course/project_2_next-ecommerce/app/product/[slug]/page.tsx
+++ Next-JS_Course/project_2_next-ecommerce/app/product/[slug]/page.tsx
@@ -2,7 +2,8 @@ import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { getProductBySlug } from "@/lib/actions";
-import { formatPrice } from "@/lib/utils";
+import { formatPrice, sleep } from "@/lib/utils";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export async function generateMetadata({
@@ -44,27 +45,41 @@ export default async function ProductPage({
     notFound();
   }
 
-  await new Promise((resolve) => setTimeout(resolve, 5000)); // Simulate delay
+  await sleep(1000);
 
   return (
     <main className="container mx-auto p-4">
-      <Card className="max-w-3xl mx-auto ">
-        <CardContent className="p-6">
-          <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
-
-          <div className="flex items-center gap-2 mb-4">
-            <span className="font-semibold text-lg">
-              {formatPrice(product.price)}
-            </span>
-
-            <Badge variant="outline">{product.category?.name}</Badge>
+      <Card className="max-w-3xl mx-auto">
+        <CardContent className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="relative rounded-lg overflow-hidden ">
+            {product.image && (
+              <Image
+                src={product.image}
+                alt={product.name}
+                fill
+                priority
+                sizes="(max-width: 768px) 100vw, 50vw"
+                className="object-cover"
+              />
+            )}
           </div>
+          <div>
+            <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
 
-          <Separator className="my-4" />
+            <div className="flex items-center gap-2 mb-4">
+              <span className="font-semibold text-lg">
+                {formatPrice(product.price)}
+              </span>
 
-          <div className="space-y-2">
-            <h2 className="font-medium">Description</h2>
-            <p>{product.description}</p>
+              <Badge variant="outline">{product.category?.name}</Badge>
+            </div>
+
+            <Separator className="my-4" />
+
+            <div className="space-y-2">
+              <h2 className="font-medium">Description</h2>
+              <p>{product.description}</p>
+            </div>
           </div>
         </CardContent>
       </Card>
--- Next-JS_Course/project_2_next-ecommerce/lib/utils.ts
+++ Next-JS_Course/project_2_next-ecommerce/lib/utils.ts
@@ -11,3 +11,7 @@ export function formatPrice(price: number): string {
     currency: "USD",
   }).format(price);
 }
+
+export async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
