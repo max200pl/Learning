bfc7582b2a92a80ec8160c793fb368229d93e66f
Maksym Poskannyi <max2000pl@gmail.com>
2026-01-08 13:27:31 +0200

    #Next_Js #eCommerce #Add navigation links and loading skeleton for product display

--- Next-JS_Course/project_2_next-ecommerce/app/page.tsx
+++ Next-JS_Course/project_2_next-ecommerce/app/page.tsx
@@ -1,9 +1,19 @@
 import { ProductCard } from "./ProductCard";
 import { prisma } from "@/lib/prisma";
 
-export default async function HomePage() {
-  const products = await prisma.product.findMany();
-  console.log("Products from DB:", products);
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+export default async function HomePage(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+
+  const page = Number(searchParams.page) || 1;
+  const pageSize = 3;
+  const skip = (page - 1) * pageSize;
+
+  const products = await prisma.product.findMany({
+    skip,
+    take: pageSize,
+  });
 
   return (
     <main className="container mx-auto p-4">
