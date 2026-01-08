d54113e2d7c50d905a670c28dc7dcd3a611cf2ba
Maksym Poskannyi <max2000pl@gmail.com>
2026-01-08 15:34:59 +0200

    #Next_Js #eCommerce #Add ProductsSkeleton component and integrate it into loading and homepage

--- Next-JS_Course/project_2_next-ecommerce/app/page.tsx
+++ Next-JS_Course/project_2_next-ecommerce/app/page.tsx
@@ -1,5 +1,6 @@
 import { ProductCard } from "./ProductCard";
 import { prisma } from "@/lib/prisma";
+
 import {
   Pagination,
   PaginationContent,
@@ -8,35 +9,48 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import { Suspense } from "react";
+import ProductsSkeleton from "./ProductsSkeleton";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-export default async function HomePage(props: { searchParams: SearchParams }) {
-  const searchParams = await props.searchParams;
+const pageSize = 3;
 
-  const page = Number(searchParams.page) || 1;
-  const pageSize = 3;
+async function Products({ page }: { page: number }) {
   const skip = (page - 1) * pageSize;
+  const products = await prisma.product.findMany({
+    skip,
+    take: pageSize,
+  });
 
-  const [products, total] = await Promise.all([
-    prisma.product.findMany({
-      skip,
-      take: pageSize,
-    }),
-    prisma.product.count(),
-  ]);
-
-  const totalPages = Math.ceil(total / pageSize);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
 
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Home</h1>
+    <>
       <p>Showing {products.length} products</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+    </>
+  );
+}
+
+export default async function HomePage(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+  const page = Number(searchParams.page) || 1;
+
+  const total = await prisma.product.count();
+  const totalPages = Math.ceil(total / pageSize);
+
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Home</h1>
+
+      <Suspense key={page} fallback={<ProductsSkeleton />}>
+        <Products page={page} />
+      </Suspense>
 
       <Pagination className="mt-8">
         <PaginationContent>
