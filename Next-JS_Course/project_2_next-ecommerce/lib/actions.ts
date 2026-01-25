0ea9a15a97ee090e6fe5bd42b0364e28e8b3787e
Maksym Poskannyi <max2000pl@gmail.com>
2026-01-25 12:24:08 +0200

    #Next_Js #eCommerce #Implement cart functionality; add Cart and CartItem models to schema; create migration for cart tables; update actions to manage cart operations.

--- Next-JS_Course/project_2_next-ecommerce/lib/actions.ts
+++ Next-JS_Course/project_2_next-ecommerce/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { Prisma } from "@/app/generated/prisma/client";
 import { prisma } from "./prisma";
+import { cookies } from "next/headers";
 
 export interface GetProductsParams {
   query?: string;
@@ -63,4 +64,98 @@ export async function getProductBySlug(slug: string) {
   if (!product) return null;
 
   return product;
+}
+
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: { items: { include: { product: true } } };
+}>;
+
+export type ShoppingCart = CartWithProducts & {
+  size: number;
+  subtotal: number;
+};
+
+async function findCartFromCookie(): Promise<CartWithProducts | null> {
+  const cartId = (await cookies()).get("cartId")?.value;
+
+  if (!cartId) {
+    return null;
+  }
+
+  const cart = await prisma.cart.findUnique({
+    where: { id: cartId },
+    include: {
+      items: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return cart;
+}
+
+export async function getCart(): Promise<ShoppingCart | null> {
+  const cart = await findCartFromCookie();
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    ...cart,
+    size: cart.items.length,
+    subtotal: cart.items.reduce(
+      (total, item) => total + item.product.price * item.quantity,
+      0
+    ),
+  };
+}
+
+async function getOrCreateCart(): Promise<CartWithProducts> {
+  let cart = await findCartFromCookie();
+
+  if (cart) {
+    return cart;
+  }
+
+  cart = await prisma.cart.create({
+    data: {},
+    include: { items: { include: { product: true } } },
+  });
+
+  (await cookies()).set("cartId", cart.id, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+
+  return cart;
+}
+
+export async function addToCart(productId: string, quantity: number = 1) {
+  if (quantity < 1) {
+    throw new Error("Quantity must be at least 1");
+  }
+
+  const cart = await getOrCreateCart();
+  const existingItem = cart.items.find((item) => item.productId === productId);
+
+  if (existingItem) {
+    await prisma.cartItem.update({
+      where: { id: existingItem.id },
+      data: { quantity: existingItem.quantity + quantity },
+    });
+  } else {
+    await prisma.cartItem.create({
+      data: {
+        cartId: cart.id,
+        productId,
+        quantity,
+      },
+    });
+  }
+
+  // Revalidate pages
 }
