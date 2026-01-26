c168acc3829f2080f656383421784db3f9491424
Maksym Poskannyi <max2000pl@gmail.com>
2026-01-26 11:37:01 +0200

    #Next_Js #eCommerce #Add CartPage and CartEntry components; implement cart display logic and item management functionality

---
+++ Next-JS_Course/project_2_next-ecommerce/app/cart/page.tsx
@@ -0,0 +1,25 @@
+import CartEntry from "@/components/cart-entry";
+import { getCart } from "@/lib/actions";
+
+export default async function CartPage() {
+  const cart = await getCart();
+
+  return (
+    <main className="container mx-auto py-4">
+      {!cart || cart.items.length === 0 ? (
+        <div className="text-center">
+          <h2 className="text-2xl">Your cart is empty</h2>
+          <p className="text-gray-500">
+            Add some items to your cart to get started.
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col">
+          {cart.items.map((item) => (
+            <CartEntry key={item.id} cartItem={item} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
+}
--- Next-JS_Course/project_2_next-ecommerce/lib/actions.ts
+++ Next-JS_Course/project_2_next-ecommerce/lib/actions.ts
@@ -76,6 +76,10 @@ export type ShoppingCart = CartWithProducts & {
   subtotal: number;
 };
 
+export type CartItemWithProduct = Prisma.CartItemGetPayload<{
+  include: { product: true };
+}>;
+
 async function findCartFromCookie(): Promise<CartWithProducts | null> {
   const cartId = (await cookies()).get("cartId")?.value;
 
